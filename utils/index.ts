90c1192266ea35fa55eb17c42e811fd80c61f626
sunly <sunjingxiang1995@gmail.com>
2023-07-13 15:12:13 +0800

    remove: drop console

--- utils/index.ts
+++ utils/index.ts
@@ -3,25 +3,25 @@
  * @Author: Sunly
  * @Date: 2023-07-11 05:02:29
  */
-const transMinToHour = (time: number) => {
+function transMinToHour(time: number): string {
   if (time > 60) {
-    const h = Math.floor(time / 60);
-    const m = time % 60;
-    return `${h}小时${m}分钟`;
+    const hours: number = Math.floor(time / 60);
+    const minutes: number = time % 60;
+    return `${hours}小时${minutes}分钟`;
   } else {
     return `${time}分钟`;
   }
-};
+}
 
 // 将毫秒数转换为天数，如果不足一天则转换为小时，如果不足一小时则转换为分钟
-const formatTime = (time: number) => {
-  if (time > 86400000) {
-    const d = Math.floor(time / 86400000);
-    const h = Math.floor((time % 86400000) / 3600000);
-    const res = `${d}天${h}小时`;
-    return res.includes('0分钟') ? res.replace('0分钟', '') : res;
+const formatTime = (t: number): string => {
+  if (t > 86400000) {
+    const days: number = Math.floor(t / 86400000);
+    const hours: number = Math.floor((t % 86400000) / 3600000);
+    const res: string = `${days}天${hours}小时`;
+    return res.includes("0分钟") ? res.replace("0分钟", "") : res;
   } else {
-    return transMinToHour(Math.floor(time / 60000));
+    return transMinToHour(Math.floor(t / 60000));
   }
 };
 
